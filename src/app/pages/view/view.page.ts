8082c14933e1c8932d331d44e2995ad694d4895a
André Luferat <contato@luferat.net>
2021-02-11 11:19:30 -0300

    Documentação atualizada

--- src/app/pages/view/view.page.ts
+++ src/app/pages/view/view.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-// 1) Importa dependências
+// 1. Importa dependências
 import { ActivatedRoute } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
@@ -12,24 +12,24 @@ import { Observable } from 'rxjs';
 })
 export class ViewPage implements OnInit {
 
-  // 3 Atributos do objeto
+  // 3. Atributos do objeto
   item: Observable<any>; // Dados do artigo
 
   constructor(
 
-    // 2) Injeta dependências
+    // 2. Injeta dependências
     private route: ActivatedRoute,
     private afs: AngularFirestore
   ) { }
 
   ngOnInit() {
 
-    // 3) Obtém Id do artigo da rota (endereço da página)
+    // 4. Obtém Id do artigo da rota (endereço da página)
     this.route.params.subscribe(
       (data) => {
         if (data.id) {
 
-          // 4) Obtém o documento do banco de dados
+          // 5. Obtém o documento do banco de dados
           this.item = this.afs.doc<any>(`articles/${data.id}`).valueChanges();
         }
       }
