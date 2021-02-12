5a67bbb3e5f0028a91f64cc3eedc330d176ce94c
André Luferat <33375030+Luferat@users.noreply.github.com>
2021-02-11 21:37:20 -0300

    Aula 02 concluída

---
+++ src/app/pages/view/view.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-view',
+  templateUrl: './view.page.html',
+  styleUrls: ['./view.page.scss'],
+})
+export class ViewPage implements OnInit {
+
+  public item: Observable<any>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private afs: AngularFirestore
+  ) { }
+
+  ngOnInit() {
+    // Obtém o id da rota
+    this.route.params.subscribe(
+      (data) => {
+        if (data.id) {
+
+          // Obtém documento do database
+          this.item = this.afs.doc<any>(`articles/${data.id}`).valueChanges();          
+        }
+      }
+    )
+  }
+}
