5b682b51adf75f3c658cee263bf8b7f9438e2ee6
André Luferat <33375030+Luferat@users.noreply.github.com>
2021-02-12 21:30:46 -0300

    Aula 03 - Autenticação Ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+// 1. Importar dependências
+import { AngularFireAuthGuard, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
+
+// 2. Define redirecionamentos
+const toLogin = () => redirectUnauthorizedTo(['/login']);
+const isLogged = () => redirectLoggedInTo(['/home']);
+
 const routes: Routes = [
 
   // 1) Define a página inicial como 'home'
@@ -11,31 +18,58 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
   },
   {
     path: 'news',
-    loadChildren: () => import('./pages/news/news.module').then( m => m.NewsPageModule)
+    loadChildren: () => import('./pages/news/news.module').then(m => m.NewsPageModule)
   },
   {
     path: 'contacts',
-    loadChildren: () => import('./pages/contacts/contacts.module').then( m => m.ContactsPageModule)
+    loadChildren: () => import('./pages/contacts/contacts.module').then(m => m.ContactsPageModule)
   },
   {
     path: 'about',
-    loadChildren: () => import('./pages/about/about.module').then( m => m.AboutPageModule)
+    loadChildren: () => import('./pages/about/about.module').then(m => m.AboutPageModule)
   },
-    {
+  {
     path: 'view/:id',
-    loadChildren: () => import('./pages/view/view.module').then( m => m.ViewPageModule)
+    loadChildren: () => import('./pages/view/view.module').then(m => m.ViewPageModule),
+
+    // Só pode ser vista se estiver logado
+    canActivate: [AngularFireAuthGuard], data: { authGuardPipe: toLogin }
+  },
+
+  // Páginas de 'login', 'logout' e 'perfil' de usuário
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule),
+
+    // Só pode ser vista se não estiver logado
+    canActivate: [AngularFireAuthGuard], data: { authGuardPipe: isLogged }
+  },
+  {
+    path: 'logout',
+    loadChildren: () => import('./pages/logout/logout.module').then(m => m.LogoutPageModule),
+
+    // Só pode ser vista se estiver logado
+    canActivate: [AngularFireAuthGuard], data: { authGuardPipe: toLogin }
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfilePageModule),
+
+    // Só pode ser vista se estiver logado
+    canActivate: [AngularFireAuthGuard], data: { authGuardPipe: toLogin }
   },
 
   // 2) Se uma rota não existe, exibe 'Erro 404'
   // DEVE SER SEMPRE A ÚLTIMA ROTA
   {
     path: '**',
-    loadChildren: () => import('./pages/e404/e404.module').then( m => m.E404PageModule)
+    loadChildren: () => import('./pages/e404/e404.module').then(m => m.E404PageModule)
   }
+
 
 ];
 
@@ -45,4 +79,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
