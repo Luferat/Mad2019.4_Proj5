0bdbf70f8c57897f3231f2180cd315f4e7ab41c9
André Luferat <contato@luferat.net>
2021-02-18 12:10:13 -0300

    Dicas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,17 @@ import { Component, OnInit } from '@angular/core';
 // 1. Importa dependências
 import { AngularFireAuth } from '@angular/fire/auth';
 
+// DICA 2.1)
+import { Plugins } from '@capacitor/core';
+const { SplashScreen } = Plugins;
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
+// DICA 2.2)
 export class AppComponent implements OnInit {
 
   private splash = true;
@@ -15,11 +21,12 @@ export class AppComponent implements OnInit {
   constructor(
 
     // 2. Injeta dependências
-    private auth: AngularFireAuth,
+    private auth: AngularFireAuth
   ) { }
 
+  // DICA 2.3)
   ngOnInit() {
-
+    SplashScreen.hide();
     const timer = setTimeout(() => { this.splash = false; }, 2000);
   }
 }
